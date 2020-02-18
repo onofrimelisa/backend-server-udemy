89ed0b0c69871e18fa3e372e9b3e31557162e2b4
onofrimelisa <mel.onofri@gmail.com>
2020-02-18 16:16:43 -0300

    cambio

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.use('/', appRoutes);
 
 
 // Escuchar peticiones
-app.listen(() => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Express server corriendo en el puerto 3000: \x1b[32m%s\x1b[0m', 'online');
 
 });
